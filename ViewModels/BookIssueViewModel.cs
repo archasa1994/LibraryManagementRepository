74539fdd5ed15eae8e6c831da3b97c9b1a1a345c
archasa1994 <archa@qburst.com>
2017-11-03 15:09:47 +0530

    Space added.

--- ViewModels/BookIssueViewModel.cs
+++ ViewModels/BookIssueViewModel.cs
@@ -15,7 +15,7 @@ namespace ViewModels
         public List<SelectListItem> BookID { get; set; }
         [Required(ErrorMessage = "Please select Issue Date", AllowEmptyStrings = false)]
         [DataType(DataType.Date)]
-        public DateTime IssueDate { get; set; }       
+        public DateTime  IssueDate { get; set; }       
         public int SelectedUserID { get; set; }
         public int SelectedBookID { get; set; }
 
